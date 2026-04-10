188ecb1e2c96b860b19a17614d96da83c335998d
nasre674 <tekomeg374@gmail.com>
2026-04-10 19:40:23 -0400

    Test de suppression d une voiture dans main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from crud_db import connecter_db
+from crud_db import connecter_db,ajouter_voiture,supprimer_voiture
+from voiture import Voiture
 
 connexion = connecter_db()
 
@@ -8,8 +9,6 @@ if connexion is not None:
 else:
     print("La connexion a échoué")
 
-from voiture import Voiture
-from crud_db import ajouter_voiture
 
 v1 = Voiture("Honda", "Civic", 2022, 22000)
 v2 = Voiture("Volkswagen", "Jetta", 2021, 21000)
@@ -20,3 +19,9 @@ ajouter_voiture(v1)
 ajouter_voiture(v2)
 ajouter_voiture(v3)
 ajouter_voiture(v4)
+
+def main():
+    supprimer_voiture(2)
+
+if __name__ == "__main__":
+    main()
