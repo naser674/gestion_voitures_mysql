396a30b5cc44b47c94c14d3fb8d5b28e7c17df87
nasre674 <tekomeg374@gmail.com>
2026-04-10 19:34:16 -0400

    Test de l ajout de 4 voitures dans la base de donnees

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ if connexion is not None:
     connexion.close()
 else:
     print("La connexion a échoué")
+
+from voiture import Voiture
+from crud_db import ajouter_voiture
+
+v1 = Voiture("Honda", "Civic", 2022, 22000)
+v2 = Voiture("Volkswagen", "Jetta", 2021, 21000)
+v3 = Voiture("Nissan", "Versa", 2020, 17000)
+v4 = Voiture("Audi", "A3", 2023, 32000)
+
+ajouter_voiture(v1)
+ajouter_voiture(v2)
+ajouter_voiture(v3)
+ajouter_voiture(v4)
